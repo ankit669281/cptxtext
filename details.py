ab6ed9f574db51a716014cf107d0a17d94ce693a
Rockas888 <105117889+Rockas888@users.noreply.github.com>
2024-03-18 22:16:23 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,8 +2,8 @@ import os
 
 api_id = 4942197
 api_hash = "13248a2c551b73193969b42194023635"
-bot_token = "6913163552:AAHy8Tmi-HYELZkViq0rUVLQ7cTWjYJoiMk"
-auth_users = [5892781710]
-sudo_user = 5892781710
+bot_token = "6857918785:AAE8O7lynXOFUYKjPXSooWkBzw89PnwpFjE"
+auth_users = [6585878012]
+sudo_user = 6585878012
 log_channel = -1002073188657
 txt_channel = -1002001856626
