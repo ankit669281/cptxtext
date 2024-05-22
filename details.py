67bec277f1d8459657da2574c304cef18b777ee8
ankit669281 <165649771+ankit669281@users.noreply.github.com>
2024-05-22 16:04:51 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 4942197
-api_hash = "13248a2c551b73193969b42194023635"
-bot_token = "6857918785:AAE8O7lynXOFUYKjPXSooWkBzw89PnwpFjE"
-auth_users = [6585878012]
-sudo_user = 6585878012
-log_channel = -1002073188657
-txt_channel = -1002001856626
+api_id = 26398147
+api_hash = "050ae2e744985b48fa37e8b2f38d7bea"
+bot_token = "6693956813:AAGOlafS6NpcDAvB2n6LIz19IixdOlPZjeg"
+auth_users = [7013332761]
+sudo_user = 7013332761
+log_channel = -2012178649
+txt_channel = -2012178649
